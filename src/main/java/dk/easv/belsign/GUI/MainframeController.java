4f6fac9fae2ed40631d8d035db03535f5876c088
GreenzQe <73607229+GreenzQe@users.noreply.github.com>
2025-05-02 11:38:20 +0200

    Small fix of UI 3

--- src/main/java/dk/easv/belsign/GUI/MainframeController.java
+++ src/main/java/dk/easv/belsign/GUI/MainframeController.java
@@ -2,8 +2,12 @@ package dk.easv.belsign.GUI;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
 
 public class MainframeController {
+    @FXML
+    private Pane mainPane;
+
     @FXML
     private void handleLogOut(ActionEvent event) {
     }
