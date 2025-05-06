7429c5d76ae072422b91b9fc97c92dc9b1deb6b0
GreenzQe <73607229+GreenzQe@users.noreply.github.com>
2025-05-06 13:19:38 +0200

    Update OperatorFrameController.java

--- src/main/java/dk/easv/belsign/GUI/OperatorFrameController.java
+++ src/main/java/dk/easv/belsign/GUI/OperatorFrameController.java
@@ -33,7 +33,7 @@ public class OperatorFrameController {
     @FXML
     private void onCapture() {
         Image snapshot = CameraHandler.getInstance().capturePic();
-
+            //commetn
         if(snapshot !=null) {
             imgFront.setImage(snapshot);
         } else {
