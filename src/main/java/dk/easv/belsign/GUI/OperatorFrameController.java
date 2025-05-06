93fcedab083182831032df94c5febb6cafe238e1
GreenzQe <73607229+GreenzQe@users.noreply.github.com>
2025-05-06 13:17:36 +0200

    Revert "Lort"
    This reverts commit 850f2d709595075c630373fd0290f71bcfb197b7.

--- src/main/java/dk/easv/belsign/GUI/OperatorFrameController.java
+++ src/main/java/dk/easv/belsign/GUI/OperatorFrameController.java
@@ -5,9 +5,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import org.bytedeco.javacv.Frame;
-import org.bytedeco.javacv.FrameGrabber;
-import org.bytedeco.javacv.OpenCVFrameGrabber;
 
 import java.awt.*;
 
@@ -35,28 +32,6 @@ public class OperatorFrameController {
 
     @FXML
     private void onCapture() {
-        try (OpenCVFrameGrabber grabber = new OpenCVFrameGrabber(0)) { // 0 for default camera
-            grabber.start(); // Start the grabber
-            Frame frame = grabber.grab(); // Grab a frame
-
-            if (frame != null) {
-                // Convert Frame to BufferedImage
-                java.awt.image.BufferedImage bufferedImage = org.bytedeco.javacv.Java2DFrameUtils.toBufferedImage(frame);
-
-                // Convert BufferedImage to JavaFX Image
-                Image fxImage = javafx.embed.swing.SwingFXUtils.toFXImage(bufferedImage, null);
-
-                // Set the image in imgFront
-                imgFront.setImage(fxImage);
-            } else {
-                System.err.println("Failed to capture frame.");
-            }
-        } catch (FrameGrabber.Exception e) {
-            e.printStackTrace();
-        }
-
-
-        /*
         Image snapshot = CameraHandler.getInstance().capturePic();
 
         if(snapshot !=null) {
@@ -64,7 +39,5 @@ public class OperatorFrameController {
         } else {
             System.err.println("Something went wrong, couldnt capture image");
         }
-
-         */
     }
 }
