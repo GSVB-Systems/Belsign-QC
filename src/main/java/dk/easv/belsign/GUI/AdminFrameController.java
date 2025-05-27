add7ff47d93c50f71c4ecc15f0ce9e9c765934ec
GreenzQe <73607229+GreenzQe@users.noreply.github.com>
2025-05-27 10:55:45 +0200

    Admin

---
+++ src/main/java/dk/easv/belsign/GUI/AdminFrameController.java
@@ -0,0 +1,13 @@
+package dk.easv.belsign.GUI;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+
+public class AdminFrameController {
+    @FXML
+    private TableView tblOrders;
+    @FXML
+    private TableView tblProducts;
+    @FXML
+    private TableView tblInformation;
+}
